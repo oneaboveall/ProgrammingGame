7c9b2bf3c59d708177333bbee98dc151bb8b6b85
oneaboveall <one.above.all.ex@gmail.com>
2013-12-03 13:22:17 -0500

    Update commanderFight.js

--- commanderFight.js
+++ commanderFight.js
@@ -2,7 +2,7 @@ function commanderFight(){
 if (cFCounter < 1){
   ID();
   player.maxhp=player.hp;
-  document.getElementById('storyDiv').innerHTML = "You are in combat with the commander...good luck!"
+  document.getElementById('storyDiv').innerHTML = "When you enter the room you encounter an enourmous ork clothed in ebony armor and sitting on a throne of human skulls! He is drinking marrow from a bone and reeks cheap alcohol! When he notices you, he lifts a gigantic cleaver and charges you. You must destroy this scum! "
   +'<br><input type="button" value=' + player.spl1 + ' onclick="CFS1()"></input>'
   +'<input type="button" value=' + player.spl2 + ' onclick="CFS2()"></input>'
   +'<input type="button" value="TEST AHP" onclick="alert(cFCounter)"></input>'
@@ -13,7 +13,7 @@ if (cFCounter < 1){
 
 function CFS1(){
   if (player.atk+d20()+50>= commander.atk+d12()){
-    document.getElementById('storyDiv').innerHTML = "You are in combat with the commander. WINNING!"
+    document.getElementById('storyDiv').innerHTML = "You use your " + player.spl1 + " ability and land it on his hideous ork face! Blood flows freely from the beasts maw!"
     +'<br><input type="button" value=' + player.spl1 + ' onclick="CFS1()"></input>'
     +'<input type="button" value=' + player.spl2 + ' onclick="CFS2()"></input>'
     +'<input type="button" value="TEST AHP" onclick="alert(commander.hp)"></input>'
@@ -23,7 +23,7 @@ function CFS1(){
     ID();
 }
     else{
-      document.getElementById('storyDiv').innerHTML = "You are in combat with the commander. NOT WINNING!"
+      document.getElementById('storyDiv').innerHTML = "The ork commander slashes you across the torso with his cleaver! You are in tremendous pain and quickly think of how unsanitary that thing probably is!"
       +'<br><input type="button" value=' + player.spl1 + ' onclick="CFS1()"></input>'
       +'<input type="button" value=' + player.spl2 + ' onclick="CFS2()"></input>'
       +'<input type="button" value="TEST AHP" onclick="alert(commander.hp)"></input>'
@@ -35,7 +35,7 @@ function CFS1(){
 
 function CFS2(){
   if (player.atk+d20()+50 >= commander.atk+d12()){
-    document.getElementById('storyDiv').innerHTML = "You are in combat with the commander. WINNING!"
+    document.getElementById('storyDiv').innerHTML = "Your mighty " + player.spl2 + " skill stuns the beast! He begins cyring like a tiny girl! Pathetic! You are winning!"
     +'<br><input type="button" value=' + player.spl1 + ' onclick="CFS1()"></input>'
     +'<input type="button" value=' + player.spl2 + ' onclick="CFS2()"></input>'
     +'<input type="button" value="TEST AHP" onclick="alert(commander.hp)"></input>'
@@ -44,7 +44,7 @@ function CFS2(){
     fDead();
     ID();}
     else{
-      document.getElementById('storyDiv').innerHTML = "You are in combat with the commander. NOT WINNING!"
+      document.getElementById('storyDiv').innerHTML = "Ze Ork Commandant retaliates with a vicious boot strike to your face! Your pride hurts more than your face, and your face hurts!"
       +'<br><input type="button" value=' + player.spl1 + ' onclick="CFS1()"></input>'
       +'<input type="button" value=' + player.spl2 + ' onclick="CFS2()"></input>'
       +'<input type="button" value="TEST AHP" onclick="alert(commander.hp)"></input>'
@@ -60,6 +60,6 @@ function fDead(){
     player.maxhp=player.maxhp+3;
     player.atk=player.atk+3;
     player.hp=player.maxhp;
-    document.getElementById('storyDiv').innerHTML = "You are in combat with the commander. He's dead jim."
+    document.getElementById('storyDiv').innerHTML = "You strike the beast with the fury of a million orphans! This furious strike evicerates the beast, leaving his bowels scattered acrosss the floor like smelly confetti. A WINNER IS YOU!"
       +'<br><input type="button" value="continue" onclick="barracks()"></input>';}
 }
