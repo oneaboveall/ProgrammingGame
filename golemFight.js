37ef4ae75b602e5787f74c67aec537bbb7512d8b
oneaboveall <one.above.all.ex@gmail.com>
2013-12-06 01:13:45 -0500

    Update golemFight.js

--- golemFight.js
+++ golemFight.js
@@ -2,7 +2,7 @@ function golemFight(){
 if (pFCounter < 1){
   ID();
   player.maxhp=player.hp
-  document.getElementById('storyDiv').innerHTML = "You are in combat with the golem...good luck!"
+  document.getElementById('storyDiv').innerHTML = "You are engaged by a rock golem! Show this walking piece of unfinished masonry whose boss!"
   +'<br><input type="button" value=' + player.spl1 + ' onclick="GFS1()"></input>'
   +'<input type="button" value=' + player.spl2 + ' onclick="GFS2()"></input>'
   +'<input type="button" value="TEST AHP" onclick="alert(pFCounter)"></input>'
@@ -13,7 +13,7 @@ if (pFCounter < 1){
 
 function GFS1(){
   if (player.atk+d20() >= golem.atk+d12()){
-    document.getElementById('storyDiv').innerHTML = "You are in combat with the golem. WINNING!"
+    document.getElementById('storyDiv').innerHTML = "You hit him in his enourmous, stupid rock head! You are winning!"
     +'<br><input type="button" value=' + player.spl1 + ' onclick="GFS1()"></input>'
     +'<input type="button" value=' + player.spl2 + ' onclick="GFS2()"></input>';
     golem.hp = golem.hp-player.atk;
@@ -21,7 +21,7 @@ function GFS1(){
     ID();
 }
     else{
-      document.getElementById('storyDiv').innerHTML = "You are in combat with the golem. NOT WINNING!"
+      document.getElementById('storyDiv').innerHTML = "You are being bested by a rock! Seriously! Fight harder!"
       +'<br><input type="button" value=' + player.spl1 + ' onclick="GFS1()"></input>'
       +'<input type="button" value=' + player.spl2 + ' onclick="GFS2()"></input>'
       +'<input type="button" value="TEST AHP" onclick="alert(golem.hp)"></input>'
@@ -33,14 +33,14 @@ function GFS1(){
 
 function GFS2(){
   if (player.atk+d20() >= golem.atk+d12()){
-    document.getElementById('storyDiv').innerHTML = "You are in combat with the golem. WINNING!"
+    document.getElementById('storyDiv').innerHTML = "You are doing well! He is coughing up what looks like gravel of some sort!"
     +'<br><input type="button" value=' + player.spl1 + ' onclick="GFS1()"></input>'
     +'<input type="button" value=' + player.spl2 + ' onclick="GFS2()"></input>';
     golem.hp = golem.hp - 3*player.atk;
     gDead();
     ID();}
     else{
-      document.getElementById('storyDiv').innerHTML = "You are in combat with the golem. NOT WINNING!"
+      document.getElementById('storyDiv').innerHTML = "This thing is barely animate! Its pretty much something that you could kick down the road on accident. How are you losing to this thing?!"
       +'<br><input type="button" value=' + player.spl1 + ' onclick="GFS1()"></input>'
       +'<input type="button" value=' + player.spl2 + ' onclick="GFS2()"></input>'
       +'<input type="button" value="TEST AHP" onclick="alert(golem.hp)"></input>'
@@ -56,6 +56,6 @@ function gDead(){
     player.maxhp=player.maxhp+3;
     player.atk=player.atk+3;
     player.hp=player.maxhp;
-    document.getElementById('storyDiv').innerHTML = "You are in combat with the golem. He's dead jim."
+    document.getElementById('storyDiv').innerHTML = "You send the rock golem back to wherever he came from. In tiny, little pieces."
       +'<br><input type="button" value="continue" onclick="golemRoom()"></input>';}
 }
