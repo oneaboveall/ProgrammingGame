49f75e394d24cdc5af07e5ae7bbb55f706551665
oneaboveall <one.above.all.ex@gmail.com>
2013-12-06 12:11:04 -0500

    Update dungeon47-end.js

--- dungeon47-end.js
+++ dungeon47-end.js
@@ -1,12 +1,12 @@
 function p47(){
-  document.getElementById('storyDiv').innerHTML = "T 47."
+  document.getElementById('storyDiv').innerHTML = "You find yourself at a three-way intersection."
   +'<br><input type="button" value="West" onclick="p13()"></input>'
   +'<input type="button" value="South" onclick="p49()"></input>'
   +'<input type="button" value="East" onclick="p51()"></input>';
 }
 
 function p48(){
-  document.getElementById('storyDiv').innerHTML = "T 48."
+  document.getElementById('storyDiv').innerHTML = "Another Corridor."
   +'<br><input type="button" value="North" onclick="p46()"></input>'
   +'<input type="button" value="East" onclick="p50()"></input>'
   +'<input type="button" value="South" onclick="p54()"></input>';
@@ -16,10 +16,10 @@ function p49(){
   if (p49Fight < 1){
     p49F();
     p49Fight++;
-      document.getElementById('storyDiv').innerHTML = "You are attacked by an orc! Hallway 49."
+      document.getElementById('storyDiv').innerHTML = "You are attacked by an orc!"
       + '<br><input type="button" value=' + player.spl1 + ' onclick="p49F()"></input>';
       }else{
-        document.getElementById('storyDiv').innerHTML = "Hallway 49."
+        document.getElementById('storyDiv').innerHTML = "You discover a large black alter glowing with a mysterious light. Do you dare enter?"
         +'<br><input type="button" value="North" onclick="p47()"></input>'
         +'<input type="button" value="Enter Black Alter" onclick="blkalter()"></input';}
 }
@@ -28,10 +28,10 @@ function p50(){
   if (p50Fight < 1){
     p50F();
     p50Fight++;
-      document.getElementById('storyDiv').innerHTML = "You are attacked by an orc! Hallway 50."
+      document.getElementById('storyDiv').innerHTML = "You are attacked by an orc!"
       + '<br><input type="button" value=' + player.spl1 + ' onclick="p50F()"></input>';
       }else{
-        document.getElementById('storyDiv').innerHTML = "Hallway 50. Magical battle"
+        document.getElementById('storyDiv').innerHTML = "You are in a hallway"
         +'<br><input type="button" value="West" onclick="p48()"></input>'
         +'<input type="button" value="East" onclick="sorcerersChamber()"></input>';}
 }
@@ -40,16 +40,16 @@ function p51(){
   if (p51Fight < 1){
     p51F();
     p51Fight++;
-      document.getElementById('storyDiv').innerHTML = "You are attacked by an orc! Hallway 51."
+      document.getElementById('storyDiv').innerHTML = "You are attacked by an orc!"
       + '<br><input type="button" value=' + player.spl1 + ' onclick="p51F()"></input>';
       }else{
-        document.getElementById('storyDiv').innerHTML = "Hallway 51. Battle."
+        document.getElementById('storyDiv').innerHTML = "You see the cross sword symbol indicating a barracks."
         +'<br><input type="button" value="West" onclick="p47()"></input>'
         +'<input type="button" value="Enter Barracks" onclick="barracks()"></input>';}
 }
 
 function p52(){
-  document.getElementById('storyDiv').innerHTML = "Corner 52"
+  document.getElementById('storyDiv').innerHTML = "You see a room seperating two chambers. Both seem to be unique within the dungeon."
   +'<br><input type="button" value="Enter Sorcerer\'s Chamber" onclick="sorcerersChamber()"></input>'
   +'<input type="button" value="Enter golem chamber" onclick="golemRoom()"></input>';
 }
@@ -58,10 +58,10 @@ function p53(){
   if (p53Fight < 1){
     p53F();
     p53Fight++;
-      document.getElementById('storyDiv').innerHTML = "You are attacked by an orc! T 53."
+      document.getElementById('storyDiv').innerHTML = "You are attacked by an orc!"
       + '<br><input type="button" value=' + player.spl1 + ' onclick="p53F()"></input>';
       }else{
-        document.getElementById('storyDiv').innerHTML = "T 53. Battle."
+        document.getElementById('storyDiv').innerHTML = "You see the cross-sword symbol indicating a barracks."
         +'<br><input type="button" value="West" onclick="p63()"></input>'
         +'<input type="button" value="Enter Barracks" onclick="barracks()"></input>'
         +'<input type="button"  value="South" onclick="p55()"></input>';}
@@ -71,23 +71,23 @@ function p54(){
   if (p54Fight < 1){
     p54F();
     p54Fight++;
-      document.getElementById('storyDiv').innerHTML = "You are attacked by an orc! Hallway 54."
+      document.getElementById('storyDiv').innerHTML = "You are attacked by an orc!"
       + '<br><input type="button" value=' + player.spl1 + ' onclick="p54F()"></input>';
       }else{
-        document.getElementById('storyDiv').innerHTML = "Hallway 54. Battle."
+        document.getElementById('storyDiv').innerHTML = "You dispatch your enemy and press forward."
         +'<br><input type="button" value="North" onclick="p48()"></input>'
         +'<input type="button" value="South" onclick="p56()"></input>';}
 }
 
 function p55(){
-  document.getElementById('storyDiv').innerHTML = "T 55."
+  document.getElementById('storyDiv').innerHTML = "A three-way intersection."
   +'<br><input type="button" value="North" onclick="p53()"></input>'
   +'<input type="button" value="East" onclick="r3()"></input>'
   +'<input type="button" value="South" onclick="p57()"></input>';
 }
 
 function p56(){
-  document.getElementById('storyDiv').innerHTML = "T 56. Bridge"
+  document.getElementById('storyDiv').innerHTML = "a three-way intersection."
   +'<br><input type="button" value="North" onclick="p54()"></input>'
   +'<input type="button" value="East" onclick="p60()"></input>'
   +'<input type="button" value="South" onclick="p58()"></input>';
@@ -95,19 +95,19 @@ function p56(){
 
 function p57(){
   if (p59Collapse < 1){
-    document.getElementById('storyDiv').innerHTML = "T 57."
+    document.getElementById('storyDiv').innerHTML = "A three-way intersection."
     +'<br><input type="button" value="West" onclick="p67()"></input>'
     +'<input type="button" value="North" onclick="p55()"></input>'
     +'<input type="button" value="South" onclick="p59()"></input>';
     } else{
         stopTimeout(p59Ctime);
-        document.getElementById('storyDiv').innerHTML = "T 57. Collapse Cannot get to p59."
+        document.getElementById('storyDiv').innerHTML = "The bridge has collapsed. You cannot go south."
         +'<br><input type="button" value="West" onclick="p67()"></input>'
         +'<input type="button" value="North" onclick="p55()"></input>';}
 }
 
 function p58(){
-  document.getElementById('storyDiv').innerHTML = "Hallway 58"
+  document.getElementById('storyDiv').innerHTML = "A non-descript hallway."
   +'<br><input type="button" value="North" onclick="p56()"></input>'
   +'<input type="button" value="South" onclick="p41()"></input>';
 }
@@ -125,20 +125,20 @@ function p59(){
 }
 
 function p60(){
-  document.getElementById('storyDiv').innerHTML = "hallway 60."
+  document.getElementById('storyDiv').innerHTML = "a non-descript hallway."
   +'<br><input type="button" value="West" onclick="p56()"></input>'
   +'<input type="button" value="East" onclick="p62()"></input>';
 }
 
 function p61(){
   if (p59Collapse < 1){
-    document.getElementById('storyDiv').innerHTML = "T 61."
+    document.getElementById('storyDiv').innerHTML = "You notice a tunnel that was once probably a secret, but no longer is."
     +'<br><input type="button" value="Not-so-secret Secret Tunnel" onclick="s3()"></input>'
     +'<input type="button" value="North" onclick="p59()"></input>'
     +'<input type="button" value="South" onclick="p33()"></input>';
     } else{
       stopTimeout(p59Ctime);
-      document.getElementById('storyDiv').innerHTML = "T 61. Collapsed, cannot get to p59."
+      document.getElementById('storyDiv').innerHTML = "The bridge is blocking the road to the north. Head south, or use the tunnel?"
       +'<br><input type="button" value="Not-so-secret Secret Tunnel" onclick="s3()"></input>'
       +'<input type="button" value="South" onclick="p33()"></input>';}
 }
@@ -147,57 +147,57 @@ function p62(){
   if (p62Fight < 1){
     p62F();
     p62Fight++;
-      document.getElementById('storyDiv').innerHTML = "You are attacked by an orc! Hallway 62."
+      document.getElementById('storyDiv').innerHTML = "You are attacked by an orc!"
       + '<br><input type="button" value=' + player.spl1 + ' onclick="p62F()"></input>';
       }else{
-        document.getElementById('storyDiv').innerHTML = "Hallway 62. Battle."
+        document.getElementById('storyDiv').innerHTML = "Which way do you go next?"
         +'<br><input type="button" value="West" onclick="p60()"></input>'
         +'<input type="button" value="East" onclick="p64()"></input>';}
 }
 
 function p63(){
-  document.getElementById('storyDiv').innerHTML = "Corner 63"
+  document.getElementById('storyDiv').innerHTML = "Head South or East?"
   +'<br><input type="button" value="South" onclick="p65()"></input>'
   +'<input type="button" value="East" onclick="p53()"></input>';
 }
 
 function p64(){
-  document.getElementById('storyDiv').innerHTML = "hallway 64."
+  document.getElementById('storyDiv').innerHTML = "A non-descript hallway."
   +'<br><input type="button" value="West" onclick="p62()"></input>'
   +'<input type="button" value="East" onclick="p66()"></input>';
 }
 
 function p65(){
-  document.getElementById('storyDiv').innerHTML = "hallway 65."
+  document.getElementById('storyDiv').innerHTML = "A non-descript hallway."
   +'<br><input type="button" value="North" onclick="p63()"></input>'
   +'<input type="button" value="South" onclick="p67()"></input>';
 }
 
 function p66(){
-  document.getElementById('storyDiv').innerHTML = "Endcap 66."
+  document.getElementById('storyDiv').innerHTML = "A dead end. The tunnel stops abrubtly, this is not the way you should be going."
   +'<br><input type="button" value="West" onclick="p64()"></input>';
 }
 
 function p67(){
-  document.getElementById('storyDiv').innerHTML = "Corner 67"
+  document.getElementById('storyDiv').innerHTML = "A non-descript hallway."
   +'<br><input type="button" value="North" onclick="p65()"></input>'
   +'<input type="button" value="East" onclick="p57()"></input>';
 }
 
 function s1(){
-  document.getElementById('storyDiv').innerHTML ="Secret1"
+  document.getElementById('storyDiv').innerHTML ="You have found a secret tunnel! You are not sure which direction is which, but your fumbling has revealed it goes deeper into the dungeon. Which way do you go?"
   +'<br><input type="button" value="Exit secret Hallway" onclick="p21()"></input>'
   +'<input type="button" value="Go deeper" onclick="s2()"></input>';
 }
 
 function s2(){
-  document.getElementById('storyDiv').innerHTML = "Secret2"
+  document.getElementById('storyDiv').innerHTML = "You are completely disoriented. Maybe this hallway was a legitimate secret after all. You have think you know which direction you are traveling in, but you are not certain."
   +'<br><input type="button" value="Go West...you think" onclick="s1()"></input>'
   +'<input type="button" value="Go East-ish" onclick="s3()"></input>';
 }
 
 function s3(){
-  document.getElementById('storyDiv').innerHTML = "Secret3."
+  document.getElementById('storyDiv').innerHTML = "Huzzah! You have found the exit... probably. Do you take the supposed exit or continue deeper into this, the most secret of tunnels?."
   +'<br><input type="button" value="Go deeper" onclick="s2()"></input>'
   +'<input type="button" value="Exit secret Hallway" onclick="p61()"></input>';
 }
